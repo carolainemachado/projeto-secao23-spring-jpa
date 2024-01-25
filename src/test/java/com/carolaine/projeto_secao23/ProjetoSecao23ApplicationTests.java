92cdb3799f78c7ce34e7fec6978569e1623ed35c
Carolaine Machado <carolainemachado12345@gmail.com>
2024-01-24 21:58:46 -0300

    Project created

---
+++ src/test/java/com/carolaine/projeto_secao23/ProjetoSecao23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolaine.projeto_secao23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSecao23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
