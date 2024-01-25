2257cfbb9350ae901a6cd838b1a8b59e5130b35e
Carolaine Machado <carolainemachado12345@gmail.com>
2024-01-24 23:31:03 -0300

    Service layer, component registration

---
+++ src/main/java/com/carolaine/projeto_secao23/services/UserService.java
@@ -0,0 +1,26 @@
+package com.carolaine.projeto_secao23.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carolaine.projeto_secao23.entities.User;
+import com.carolaine.projeto_secao23.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
